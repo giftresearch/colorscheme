6b7b90a5f8bbbafb6e85f7d004466490dff0e707
Tim Case <tim@endpoint.com>
2013-03-30 21:25:01 -0700

    Add requires for color algorithms

--- lib/colorscheme.rb
+++ lib/colorscheme.rb
@@ -1,4 +1,10 @@
 require "colorscheme/version"
 require "colorscheme/colorscheme"
+require "colorscheme/analogous"
+require "colorscheme/complementary"
+require "colorscheme/split_complementary"
+require "colorscheme/squared"
+require "colorscheme/triadic"
+
 module Colorscheme
 end
